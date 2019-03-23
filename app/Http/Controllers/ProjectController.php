2f752fe8147a56ab537f3d9ae53b8eda82dc8b64
TOBEXSAM <tobexbella@gmail.com>
2019-03-23 21:04:47 +0100

    Create ProjectController.php

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    //this function saves project name into database
+
+    public function createProject(Request $request)
+    {
+        return redirect('/');
+//        $this->validate($request, [
+//            'project_title' => 'required',
+//        ]);
+//
+//        $project = \App\project::findorfail(Input::get('id'));
+//        \App\project::Create($request->all());
+//        session()->flash('message','project created Successfully');
+//        session()->flash('alert','info');
+//        return redirect('/home');
+
+
+    }
+
+}
